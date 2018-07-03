5dc644d969dc38b03d136638a3b87d8260440861
Evelina Borgia <evelinaborgia@gmail.com>
2018-07-03 15:03:19 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": ["jasmine"],
+
+    'rules': {
+        'func-names': 0,
+        
+      },
+    "env": {
+        "jasmine": true,
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jquery": true
+      },
+};
+
